16287254a7886313c1efb0dbbea151aec021bcee
Andi Bašić <andi.basic10@gmail.com>
2022-01-26 17:45:51 +0100

    Fetch and display data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
+import UserResults from "../components/users/UserResults";
+
 function Home() {
     return <div>
-        <h1 className="text-6xl">Welcome</h1>
+        <h1 className="">
+            {/* search component */}
+            <UserResults />
+        </h1>
     </div>;
 }
 
