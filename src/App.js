14ed646befdad3500f34a3226d8728769706adad
and1son <andi.basic10@gmail.com>
2022-01-26 14:40:43 +0100

    Footer component

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
-        <main>
+        <main className='container mx-auto px-3 pb-12'>
           Content
         </main>
+        <Footer />
       </div>
-      <Footer />
     </Router>
   );
 }
