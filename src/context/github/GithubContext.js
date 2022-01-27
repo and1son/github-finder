69e99253b61f7451de66acb162aa7a2660852206
and1son <andi.basic10@gmail.com>
2022-01-27 11:40:23 +0100

    fetch users cleanup

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -10,14 +10,14 @@ export const GithubProvider = ({ children }) => {
 
     const initialState = {
         users: [],
-        loading: true,
+        loading: false,
     }
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
-
-
+    //get initial users (for testing)
     const fetchUsers = async () => {
+        setLoading()
         const response = await fetch(`${GITHUB_URL}/users`, {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`,
@@ -25,11 +25,16 @@ export const GithubProvider = ({ children }) => {
         })
         const data = await response.json()
 
-
         dispatch({
             type: 'GET_USERS',
             payload: data
         })
+    }
+
+    //set loading
+
+    const setLoading = () => {
+        dispatch({ type: 'SET_LOADING' })
     }
 
     return (
